63ba5c55faeb1032b14b8aa7240d087fc0f6875e
Anatoly Politsin <politsin@gmail.com>
2017-08-29 10:09:13 +0300

    dir

--- build.py
+++ build.py
@@ -3,7 +3,5 @@
 import os
 if os.path.exists('/d/temp')
   os.chdir('/opt/docker-backup')
-else
-  os.chdir('politsin/docker-backup')
 os.system('docker build -t docker-backup:1.3 .')
 os.system('docker tag docker-backup:1.3 docker-backup:latest')
