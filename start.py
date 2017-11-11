a8b9c866d64d073f23b392bb5e814a2c1ec74a55
Anatoly Politsin <politsin@gmail.com>
2017-11-12 00:53:33 +0300

    stage

--- start.py
+++ start.py
@@ -8,40 +8,33 @@ import string
 import random
 import subprocess
 
-debug = 1;
+# Vars
 siteroot = "/var/www/html"
 settings = siteroot + "/sites/default/settings.php"
-restore_tar_options = ""
-
-# DB Defaults:
-db = 'mysql'
-dbdump = 'drush' # opts: ['drush', 'mysql', 'postgre']
-dbfile = siteroot + "/.db.sql"
-dbname = 'drupal'
-dbuser = 'drupal'
-dbpass = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
-dbhost = 'localhost'
 dbskip = "--structure-tables-list=cache,cache_*,cachetags,search_*,watchdog,history,sessions"
-dbrestore = 'false'
+tar_opts = "--exclude=/var/www/html/cmd-* --exclude=/var/www/html/adminer.php --exclude=.git"
+pass8 = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8));
+tar_opts_restore = ""
 
-if debug:
-    restore = 1
-    dbrestore = 'true'
-    backup_name = 'bcp-d-704-s-kukuska'
-    backup_path = '/var/www/html'
-    tar_options = "--exclude=/var/www/html/cmd-* --exclude=/var/www/html/adminer.php --exclude=.git"
-    aws_region = 'eu-west-1'
-    bucket_name = 'docker-bcp-daily'
-else:
-    restore = os.environ['RESTORE']
-    dbdump = os.environ['DBDUMP']
-    dbfile = os.environ['DBFILE']
-    dbskip = os.environ['DBSKIP']
-    backup_name = os.environ['BACKUP_NAME']
-    backup_path = os.environ['BACKUP_PATHS']
-    tar_options = os.environ['BACKUP_TAR_OPTION']
-    aws_region = os.environ['AWS_DEFAULT_REGION']
-    bucket_name = os.environ['S3_BUCKET_NAME']
+# Backup
+restore = os.getenv('RESTORE', 0) #['', 'restore']
+backup_name = os.getenv('BACKUP_NAME', '')
+backup_path = os.getenv('BACKUP_PATHS', '/var/www/html')
+tar_options = os.getenv('BACKUP_TAR_OPTION', tar_opts)
+
+# DB Settings:
+dbfile = os.getenv('DBFILE', siteroot + "/.db.sql")
+dbdump = os.getenv('DBDUMP', 'drush') # opts: ['', 'drush', 'mysql', 'postgre']
+dbname = os.getenv('DBNAME', 'drupal')
+dbuser = os.getenv('DBUSER', 'drupal')
+dbpass = os.getenv('DBPASS', pass8)
+dbhost = os.getenv('DBHOST', 'localhost')
+dbskip = os.getenv('DBSKIP', '')
+dbrestore = os.getenv('DBRESTORE', 'mysql') #['', 'mysql', 'postgre']
+
+# AWS Settings
+aws_region = os.getenv('AWS_DEFAULT_REGION', 'eu-west-1')
+bucket = os.getenv('S3_BUCKET_NAME', '') 
 
 def logMsg(msg):
     print (msg)
@@ -78,7 +71,7 @@ def backup():
             sendLog ('FAIL', 'Function is not ready yet, TODO')
             sys.exit()
         # Fix dump owner
-        os.system("chown www-data:www-data /var/www/html/.db.sql")
+        os.system("chown www-data:www-data %s" % (dbfile))
 
     # Create tarball
     backup_suffix = time.strftime(".%Y-%m-%d-%H-%M-%S.tar.gz", time.gmtime())
@@ -89,11 +82,11 @@ def backup():
 
     # Clean db file.
     if dbdump == 'drupal' and dbfile == "/var/www/html/.db.sql":
-        rmdump = os.system("rm -f '/var/www/html/.db.sql'")
+        rmdump = os.system("rm -f '/var/www/html/.db.sql'") # this file, not %s dbfile
         execLog(rmdump, 'OK: rm %s' % (dbfile), 'ERROR: Failed to rm %s' % (dbfile))
 
     # Upload the backup to S3 with timestamp
-    s3 = os.system("aws s3 --region %s cp %s s3://%s/%s" % (aws_region, tarball, bucket_name, tarball))
+    s3 = os.system("aws s3 --region %s cp %s s3://%s/%s" % (aws_region, tarball, bucket, tarball))
     execLog(s3, 'OK: s3 upload', 'ERROR: Failed to upload backup to AWS')
 
     # Remove old files.
@@ -108,7 +101,7 @@ def restore():
     print ("Restore")
 
     # Find Last Backup Name (Name + exec->trim->explode->arr[1])
-    cmd = "aws s3 ls s3://%s | grep %s | sort -r | head -n1" % (bucket_name, backup_name)
+    cmd = "aws s3 ls s3://%s | grep %s | sort -r | head -n1" % (bucket, backup_name)
     backup = backup_name + os.popen(cmd).read().strip("\n").split(backup_name)[1]
     bcpfail = 0;
     if len(backup) < 10:
@@ -116,11 +109,11 @@ def restore():
     execLog(bcpfail, 'Last Backup Name: %s' % (backup), 'ERROR: Failed to get Last Backup Name')
 
     # Download from AWS S3
-    s3 = os.system("aws s3 cp --region %s s3://%s/%s %s" % (aws_region, bucket_name, backup, backup))
+    s3 = os.system("aws s3 cp --region %s s3://%s/%s %s" % (aws_region, bucket, backup, backup))
     execLog(s3, 'OK: Download Last Backup from AWS', 'ERROR: Failed to download last backup from AWS')
 
     # Un TAR
-    untar = os.system("tar xzf %s %s" % (backup, restore_tar_options))
+    untar = os.system("tar xzf %s %s" % (backup, tar_opts_restore))
     execLog(untar, 'OK: Unpack backup', 'ERROR: Failed to unpack Last Backup')
 
     # Write settings php
@@ -131,13 +124,13 @@ def restore():
             myfile.write("\n$databases['default']['default']['password'] = '%s';\n" % (dbpass))
 
     # Restore db
-    if dbrestore == 'true' and len(dbpass) >=6 :
+    if len(dbrestore) > 4:
         # Mysql db-restore.
-        if db == 'mysql':
+        if dbrestore == 'mysql' and len(dbpass) >=3:
             mysql = os.system("mysql -u %s -p%s %s < %s" % (dbuser, dbpass, dbname, dbfile))
             execLog(mysql, 'OK: Restore DB dump', 'ERROR: Failed to restore DB dump')
         # TODO: Postgre db-restore.
-        if db == 'postgre':
+        if dbrestore == 'postgre':
             sendLog ('FAIL', 'Function is not ready yet, TODO')
             sys.exit()
 
@@ -145,7 +138,7 @@ def restore():
     sendLog ('OK', 'Restore Completed')
     return;
 
-if restore:
+if restore == 'restore':
     restore()
 else:
     backup()
