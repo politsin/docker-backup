422bbc6d18797c456d6476fadaf689af913d3d78
Anatoly Politsin <politsin@gmail.com>
2017-11-20 16:00:57 +0300

    mysql & postgre dump

--- start.py
+++ start.py
@@ -85,7 +85,7 @@ def post_to_mattermost(webhook, status, backup_name, msg):
 
 def backup():
     # Say 'Start'
-    sendLog ('START BCP', 'Backup start')
+    sendLog ('START BCP', 'Backup start [%s]' % (backup_path))
     print ("Backup")
 
     if len(tz) > 4:
@@ -95,17 +95,21 @@ def backup():
     if len(dbdump) > 4:
         # Create drush drupal-DBdump
         if dbdump == 'drush':
-            drushdump = "/usr/local/bin/drush sql-dump --root=%s" % (siteroot)
-            backup = os.system("%s --result-file=%s %s" % (drushdump, dbfile, dbskip))
-            execLog(backup, 'OK: Dump DB', 'ERROR: Failed to create DB dump')
-        # TODO: Create mysqml-DBdump
-        if dbdump == 'mysqml':
-            sendLog ('FAIL', 'Function is not ready yet, TODO')
-            sys.exit()
-        # TODO: Create postgre-DBdump
+            drush = "/usr/local/bin/drush --root=%s" % (siteroot)
+            drushdump = os.system("%s sql-dump --result-file=%s %s" % (drush, dbfile, dbskip))
+            execLog(drushdump, 'OK: DRUSH db-dump', 'ERROR: Failed to create DRUSH db-dump')
+        # Create mysqml-DBdump
+        if dbdump == 'mysql':
+            mysqldump = os.system("mysqldump -u%s -p%s -h%s %s > %s" % (dbuser, dbpass, dbhost, dbname, dbfile))
+            execLog(mysqldump, 'OK: MySQL db-dump', 'ERROR: Failed to create MySQL db-dump')
+        # Create postgre-DBdump
         if dbdump == 'postgre':
-            sendLog ('FAIL', 'Function is not ready yet, TODO')
-            sys.exit()
+            print "pg_dump -U=%s -h=%s %s > %s" % (dbuser, dbhost, dbname, dbfile)
+            host = ''
+            if len(dbhost) > 4 and dbhost != 'localhost':
+                host = "-h %s" % (dbhost)
+            pgdump = os.system("pg_dump -U %s %s %s > %s" % (dbuser, host, dbname, dbfile))
+            execLog(pgdump, 'OK: PostgreSQL db-dump', 'ERROR: Failed to create PostgreSQL db-dump')
         # Fix dump owner
         os.system("chown www-data:www-data %s" % (dbfile))
 
@@ -118,7 +122,7 @@ def backup():
     execLog(tar, 'OK: tar files', 'ERROR: Failed to create tar files')
 
     # Clean db file.
-    if dbdump == 'drush' and dbfile == "/var/www/html/.db.sql":
+    if len(dbdump) > 4 and dbfile == "/var/www/html/.db.sql":
         rmdump = os.system("rm -f '/var/www/html/.db.sql'") # this file, not %s dbfile
         execLog(rmdump, 'OK: rm %s' % (dbfile), 'ERROR: Failed to rm %s' % (dbfile))
 
