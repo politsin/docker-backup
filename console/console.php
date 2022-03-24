ce95b1821c7c67a0b3c038bbe2289ea98ad51551
Anatoly <politsin@gmail.com>
2022-03-24 15:47:39 +0300

    Update console.php

--- console/console.php
+++ console/console.php
@@ -9,13 +9,17 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
+use App\Command\S3Backup;
 
 // Sup .env vars.
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/.env');
 
 // Symfony app.
-$app = new Application('Console App', 'v1.2.3');
-print_r($_ENV);
+$app = new Application('Console App', 'v0.1.0');
+$app->add(new S3Backup());
+if (!empty($_ENV['TEMPLATE'])) {
+  $app->setDefaultCommand($_ENV['TEMPLATE'], TRUE);
+}
 // Run.
 $app->run();
