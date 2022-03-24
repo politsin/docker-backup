3d7b7c47505000493616ae719c4336c6cfc79334
Anatoly <politsin@gmail.com>
2022-03-24 15:42:58 +0300

    Update console.php

--- console/console.php
+++ console/console.php
@@ -1,2 +1,21 @@
+#!/usr/bin/env php
 <?php
-print "hello";
+
+if (!is_dir(__DIR__ . "/vendor")) {
+  shell_exec("composer install --no-dev  -o -d " . __DIR__);
+}
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Dotenv\Dotenv;
+
+// Sup .env vars.
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/.env');
+
+// Symfony app.
+$app = new Application('Console App', 'v1.2.3');
+print_r($_ENV);
+// Run.
+$app->run();
