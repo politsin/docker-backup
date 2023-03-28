5cc0c526ff45ef46457705b09bc71ba737162820
Anatoly Politsin <politsin@gmail.com>
2023-03-28 10:44:23 +0300

    x

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# cd /opt/apps/bridge
-docker build . -t synst/s3-dockup
+# cd /opt/build/docker-backup
+docker build . -t synstd/s3-dockup
+docker tag synstd/s3-dockup synstd/s3-dockup:8.1.0
+# docker push synstd/s3-dockup
+# docker push synstd/s3-dockup:8.1.0
